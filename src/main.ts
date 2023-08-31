0a558836779265fccf19cc1ddb1874d75bfb25a1
alekseiv25 <alekseiv25@gmail.com>
2023-08-31 05:13:02 -0700

    JS-4 set-up backend

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,7 @@ import { AppModule } from './app.module';
 async function start() {
   const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(PORT, () =>
-    console.log(`JSLab-backend server started on port ${PORT}`),
-  );
+  await app.listen(PORT, () => console.log(`JSLab-backend server started on port ${PORT}`));
 }
 
 start();
