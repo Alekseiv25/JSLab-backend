ee209a1ddf6b718e2ab177bbaf449dae3b8bdd3b
alekseiv25 <alekseiv25@gmail.com>
2023-08-31 06:32:26 -0700

    example

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function start() {
   const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
   await app.listen(PORT, () => console.log(`JSLab-backend server started on port ${PORT}`));
-  console.log();
+  console.log(123123123);
 }
 
 start();
