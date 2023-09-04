398dbb9c0bd495d4545da11bc56ff4bc3be280fa
Andrei Shket <andrewshkt@gmail.com>
2023-09-04 21:17:08 +0300

    Added PUT, DELETE and GET by ID

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Get, Body } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Body, Param } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 
@@ -6,13 +6,28 @@ import { CreateUserDto } from './dto/create-user.dto';
 export class UsersController {
   constructor(private userService: UsersService) {}
 
+  @Get()
+  getAll() {
+    return this.userService.getAllUsers();
+  }
+
+  @Get(':id')
+  getUserByID(@Param('id') id: string) {
+    return this.userService.getUserByID(id);
+  }
+
   @Post()
   create(@Body() userDto: CreateUserDto) {
     return this.userService.createUser(userDto);
   }
 
-  @Get()
-  getAll() {
-    return this.userService.getAllUsers();
+  @Put(':id')
+  updateUserByID(@Param('id') id: string, @Body() updatedUserDto: CreateUserDto) {
+    return this.userService.updateUserByID(id, updatedUserDto);
+  }
+
+  @Delete(':id')
+  deleteUserByID(@Param('id') id: string) {
+    return this.userService.deleteUserByID(id);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
 import { User } from './users.model';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -7,13 +7,45 @@ import { CreateUserDto } from './dto/create-user.dto';
 export class UsersService {
   constructor(@InjectModel(User) private userRepository: typeof User) {}
 
+  async getAllUsers() {
+    const users = await this.userRepository.findAll();
+    return users;
+  }
+
+  async getUserByID(id: string) {
+    const user = await this.userRepository.findByPk(id);
+
+    if (!user) {
+      throw new NotFoundException(`User with ID ${id} not found`);
+    }
+
+    return user;
+  }
+
   async createUser(dto: CreateUserDto) {
     const user = await this.userRepository.create(dto);
     return user;
   }
 
-  async getAllUsers() {
-    const users = await this.userRepository.findAll();
-    return users;
+  async updateUserByID(id: string, updatedUserDto) {
+    const user = await this.userRepository.findByPk(id);
+
+    if (!user) {
+      throw new NotFoundException(`User with ID ${id} not found`);
+    }
+
+    await user.update(updatedUserDto);
+    return user;
+  }
+
+  async deleteUserByID(id: string) {
+    const user = await this.userRepository.findByPk(id);
+
+    if (!user) {
+      throw new NotFoundException(`User with ID ${id} not found`);
+    }
+
+    await user.destroy();
+    return { message: `User with ID ${id} has been deleted...` };
   }
 }
