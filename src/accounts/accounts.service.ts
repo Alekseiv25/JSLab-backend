afcc5f783a9831f8695d86716af71af1e274d1f3
alekseiv25 <alekseiv25@gmail.com>
2023-10-19 19:36:33 +0300

    add decryipting at getaccountsbybusinessid, refactor code

--- src/accounts/accounts.service.ts
+++ src/accounts/accounts.service.ts
@@ -15,29 +15,41 @@ import { Op } from 'sequelize';
 
 @Injectable()
 export class AccountsService {
-  key: Buffer;
+  key32: Buffer;
+  key16: Buffer;
   constructor(@InjectModel(Account) private accountRepository: typeof Account) {
-    this.key = this.loadEncryptionKey();
+    const { key32, key16 } = this.loadEncryptionKeys();
+    this.key32 = key32;
+    this.key16 = key16;
   }
 
-  private generateEncryptionKey(): Buffer {
-    return crypto.randomBytes(32);
+  private generateEncryptionKeys(): { key32: Buffer; key16: Buffer } {
+    const key32 = crypto.randomBytes(32);
+    const key16 = crypto.randomBytes(16);
+    return { key32, key16 };
   }
 
-  private saveEncryptionKey(key: Buffer): void {
-    const config = { key32: key.toString('hex') };
+  private saveEncryptionKeys(keys: { key32: Buffer; key16: Buffer }): void {
+    const config = {
+      key32: keys.key32.toString('hex'),
+      key16: keys.key16.toString('hex'),
+    };
     fs.writeFileSync('keys.json', JSON.stringify(config));
   }
 
-  private loadEncryptionKey(): Buffer {
+  private loadEncryptionKeys(): { key32: Buffer; key16: Buffer } {
     try {
       const config = JSON.parse(fs.readFileSync('keys.json', 'utf8'));
-      const keyString = config.key32;
-      return Buffer.from(keyString, 'hex');
+      const key32String = config.key32;
+      const key16String = config.key16;
+      return {
+        key32: Buffer.from(key32String, 'hex'),
+        key16: Buffer.from(key16String, 'hex'),
+      };
     } catch (error) {
-      const newKey = this.generateEncryptionKey();
-      this.saveEncryptionKey(newKey);
-      return newKey;
+      const newKeys = this.generateEncryptionKeys();
+      this.saveEncryptionKeys(newKeys);
+      return newKeys;
     }
   }
 
@@ -49,19 +61,20 @@ export class AccountsService {
     }
 
     const decryptedAccounts = accounts.map((account) => {
-      const decryptedRoutingNumber = decrypt(account.routingNumber, this.key);
-      const decryptedAccountNumber = decrypt(account.accountNumber, this.key);
-      return {
+      const decryptedRoutingNumber = decrypt(account.routingNumber, this.key32, this.key16);
+      const decryptedAccountNumber = decrypt(account.accountNumber, this.key32, this.key16);
+      const decryptedAccount = Account.build({
         ...account.toJSON(),
         routingNumber: decryptedRoutingNumber,
         accountNumber: decryptedAccountNumber,
-      };
+      });
+      return decryptedAccount;
     });
     return decryptedAccounts;
   }
 
   async getAccountsByBusinessId(businessId: number): Promise<IGetAllAccountsResponse> {
-    const accounts: Account[] = await this.accountRepository.findAll({
+    const accounts: Account[] | null = await this.accountRepository.findAll({
       where: {
         businessId: {
           [Op.in]: [businessId],
@@ -69,19 +82,30 @@ export class AccountsService {
       },
     });
 
-    if (!accounts) {
+    if (accounts.length === 0) {
       throw new HttpException(makeNotFoundMessage('Accounts'), HttpStatus.NOT_FOUND);
     }
 
-    const response: IGetAllAccountsResponse = { status: HttpStatus.OK, data: accounts };
+    const decryptedAccounts = accounts.map((account) => {
+      const decryptedRoutingNumber = decrypt(account.routingNumber, this.key32, this.key16);
+      const decryptedAccountNumber = decrypt(account.accountNumber, this.key32, this.key16);
+      const decryptedAccount = Account.build({
+        ...account.toJSON(),
+        routingNumber: decryptedRoutingNumber,
+        accountNumber: decryptedAccountNumber,
+      });
+      return decryptedAccount;
+    });
+
+    const response: IGetAllAccountsResponse = { status: HttpStatus.OK, data: decryptedAccounts };
     return response;
   }
 
   async createNewAccount(dto: CreateAccountDto): Promise<IBasicAccountResponse> {
     const encryptedDto = {
       ...dto,
-      routingNumber: encrypt(dto.routingNumber, this.key),
-      accountNumber: encrypt(dto.accountNumber, this.key),
+      routingNumber: encrypt(dto.routingNumber, this.key32, this.key16),
+      accountNumber: encrypt(dto.accountNumber, this.key32, this.key16),
     };
 
     const newAccount: Account = await this.accountRepository.create(encryptedDto);
--- src/utils/crypto.ts
+++ src/utils/crypto.ts
@@ -1,44 +1,14 @@
 import * as crypto from 'crypto';
-import * as fs from 'fs';
 const algorithm = 'aes-256-cbc';
 
-function generateEncryptionKey(): Buffer {
-  return crypto.randomBytes(16);
-}
-
-function saveEncryptionKey(key: Buffer): void {
-  try {
-    const config = JSON.parse(fs.readFileSync('keys.json', 'utf8'));
-    config.key16 = key.toString('hex');
-    fs.writeFileSync('keys.json', JSON.stringify(config));
-    console.log('Encryption key saved.');
-  } catch (error) {
-    console.error('Error saving encryption key:', error);
-  }
-}
-
-function loadEncryptionKey(): Buffer {
-  try {
-    const config = JSON.parse(fs.readFileSync('keys.json', 'utf8'));
-    const keyString = config.key16;
-    return Buffer.from(keyString, 'hex');
-  } catch (error) {
-    const newKey = generateEncryptionKey();
-    saveEncryptionKey(newKey);
-    return newKey;
-  }
-}
-
-const iv = loadEncryptionKey();
-
-export function encrypt(text: string, key: Buffer): string {
+export function encrypt(text: string, key: Buffer, iv: Buffer): string {
   const cipher = crypto.createCipheriv(algorithm, key, iv);
   let encrypted = cipher.update(text, 'utf8', 'hex');
   encrypted += cipher.final('hex');
   return encrypted;
 }
 
-export function decrypt(encryptedText: string, key: Buffer): string {
+export function decrypt(encryptedText: string, key: Buffer, iv: Buffer): string {
   const decipher = crypto.createDecipheriv(algorithm, key, iv);
   let decrypted = decipher.update(encryptedText, 'hex', 'utf8');
   decrypted += decipher.final('utf8');
