8cca59e3ca2bbf8f46b90c083358a207c5d2357f
Andrei Shket <andrewshkt@gmail.com>
2023-11-21 14:54:06 +0300

    add: AuthGuard for support controller

--- src/support/support.controller.ts
+++ src/support/support.controller.ts
@@ -1,13 +1,15 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { SupportService } from './support.service';
 import { ISupportResponse } from 'src/types/responses/support';
 import { ISupportRequest } from 'src/types/requests/support';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @Controller('support')
 export class SupportController {
   constructor(private readonly supportService: SupportService) {}
 
   @Post()
+  @UseGuards(AuthGuard)
   async handleSupportRequest(@Body() requestBody: ISupportRequest): Promise<ISupportResponse> {
     const { userID, userMessage } = requestBody;
     const response: ISupportResponse = await this.supportService.sendEmails(userID, userMessage);
--- src/support/support.module.ts
+++ src/support/support.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { SupportController } from './support.controller';
 import { SupportService } from './support.service';
 import { UsersModule } from 'src/users/users.module';
+import { TokensModule } from 'src/tokens/tokens.module';
 
 @Module({
   controllers: [SupportController],
   providers: [SupportService],
-  imports: [UsersModule],
+  imports: [UsersModule, TokensModule],
 })
 export class SupportModule {}
